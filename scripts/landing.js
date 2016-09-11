a1dfa2458f1e29d12c10b92ee55d1e89cae3dbb7
Sharrell Porter <SharrellPorter@example.com>
2016-09-11 18:44:45 -0400

    finished

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,33 +1,15 @@
 var pointsArray = document.getElementsByClassName('point');
-
-var animatePoints = function(points) {
                 
-                var revealFirstPoint = function() {
-                    points[0].style.opacity = 1;
-                    points[0].style.transform = "scaleX(1) translateY(0)";
-                    points[0].style.msTransform = "scaleX(1) translateY(0)";
-                    points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-                };
-                
-                var revealSecondPoint = function() {
-                    points[1].style.opacity = 1;
-                    points[1].style.transform = "scaleX(1) translateY(0)";
-                    points[1].style.msTransform = "scaleX(1) translateY(0)";
-                    points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-                };
-                
-                var revealThirdPoint = function() {
-                    points[2].style.opacity = 1;
-                    points[2].style.transform = "scaleX(1) translateY(0)";
-                    
-                    points[2].style.mstransform = "scaleX(1) translateY(0)";
-                    
-                    points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-                };
-                revealFirstPoint();
-                revealSecondPoint();
-                revealThirdPoint();
-            };
+        var revealPoint = function(point) {
+            points.style.opacity = 1;
+            points.style.transform = "scaleX(1) translateY(0)";
+            points.style.msTransform = "scaleX(1) translateY(0)";
+            points.style.WebkitTransform = "scaleX(1) translateY(0)";
+        };
+        var animatePoints = function(points) {
+            forEach(points, revealPoint);    
+        };
+               
 window.onload = function() {
     if (window.innerHeight > 950) {
         animatePoints(pointsArray);
