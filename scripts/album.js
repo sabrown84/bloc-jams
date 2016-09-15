d68af45e03457b661b2c646e2e28584666344993
Sharrell Porter <SharrellPorter@example.com>
2016-09-15 09:50:27 -0400

    minor fix

--- scripts/album.js
+++ scripts/album.js
@@ -30,12 +30,12 @@ var albumMarconi = {
 
 var createSongRow = function(songNumber, songName, songLength) {
     var template = 
-        '<tr class="album-view-song-item">'
-        '   <td class="song-item-number">' + songNumber + '</td>'
-        '   <td class="song-item-title">' + songName + '</td>'
-        '   <td class="song-item-duration">' + songLength + '</td>'
-        '</tr>'
-        ;
+         '<tr class="album-view-song-item">'
+       + '  <td class="song-item-number">' + songNumber + '</td>'
+       + '  <td class="song-item-title">' + songName + '</td>'
+       + '  <td class="song-item-duration">' + songLength + '</td>'
+       + '</tr>'
+       ;
     
         return template;
 };
